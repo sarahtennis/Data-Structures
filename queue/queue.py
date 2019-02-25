ef16db9d46d7acf188724b0ac63f9fba1de46edc
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-02-25 15:17:38 -0500

    completes queue.py

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,23 @@
 class Queue:
   def __init__(self):
     self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = 
+    self.storage = []
 
+  # add an item to the back of the queue
   def enqueue(self, item):
-    pass
+    self.storage.append(item)
+    self.size += 1
   
+  # remove and return an item from the front of the queue
   def dequeue(self):
-    pass
+    if self.len():
+      front = self.storage[0]
+      del self.storage[0]
 
+      self.size -= 1
+
+      return front
+
+  #returns the number of items in the queue
   def len(self):
-    pass
+    return self.size
